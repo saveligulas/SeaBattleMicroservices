c48c4e40dc69831f09795ea47f24912eb9bf2188
Saveli Gulas <saveli.gulas@gmail.com>
2025-03-31 00:12:16 +0200

    added interface for Frames

---
+++ broker/src/main/java/sg/quarkus/amqp091/protocol/frame/IFrame.java
@@ -0,0 +1,13 @@
+package sg.quarkus.amqp091.protocol.frame;
+
+public interface IFrame {
+    byte PAYLOAD_END_BYTE = (byte) 0xCE;
+
+    int getTotalSize();
+    int getTotalPayloadSize();
+    default int getPayloadSize() {
+        return getTotalPayloadSize() - 1; //subtract the ending byte from the total payload size to get the payload size without the ending byte
+    } //returns the payload size without the ending byte
+    byte[] getData();
+    Header getHeader();
+}
