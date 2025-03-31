6931ab94edba25ebffe5c0a35d895c38f27dc359
saveligulas <saveli.gulas@gmail.com>
2025-03-31 23:38:51 +0200

    TODO: change this in future

--- broker/src/main/java/sg/quarkus/amqp091/protocol/frame/MethodFrame.java
+++ broker/src/main/java/sg/quarkus/amqp091/protocol/frame/MethodFrame.java
@@ -1,5 +1,28 @@
 package sg.quarkus.amqp091.protocol.frame;
 
-public class MethodFrame {
+import sg.quarkus.amqp091.protocol.frame.data.AMQPDataField;
+import sg.quarkus.amqp091.protocol.frame.error.UnprocessableFrameException;
 
+import java.util.List;
+
+public class MethodFrame extends AbstractFrame implements IFramePayloadClass, IFramePayloadMethod, IFramePayloadFields {
+
+    public MethodFrame(byte[] data) throws UnprocessableFrameException {
+        super(data);
+    }
+
+    @Override
+    public int getClassId() {
+        return NumberUtils.toShort(data[7], data[8]);
+    }
+
+    @Override
+    public int getMethodId() {
+        return NumberUtils.toShort(data[9], data[10]);
+    }
+
+    @Override
+    public List<AMQPDataField> getDataFields() {
+        return List.of();
+    }
 }
